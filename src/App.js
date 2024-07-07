a13e0fc7f8e4b46e2e21ccff5d6e39af444abe59
0MadGaming0 <madhavsp05@gmail.com>
2024-07-07 13:07:42 +0530

    User list

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddUser from './components/AddUser';
+import SearchUser from './components/SearchUser';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<AddUser/>}/>
+      <Route path="/search" element={<SearchUser/>}/>
     </Routes>
     </BrowserRouter>
   );
